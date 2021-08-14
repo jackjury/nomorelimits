be2795b11128bd16ae3df8f5fbad8c1ae750dfc6
jackjury <jackjury@Jacks-MacBook-Pro-2.local>
2021-08-14 13:09:13 +0100

    added functions

--- index.js
+++ index.js
@@ -7,34 +7,33 @@ const cors = require("cors");
 app.use(cors());
 
 let cache = {};
-const TIME_TO_LIVE = 300000; // 5 minutes
+const TIME_TO_LIVE = 3000; // 5 minutes
 
 app.use(express.static("public"));
 
 app.get("/apiproxy", async (req, res) => {
-  if (!req.query.url) {
-    // Serve Static HTML
-    console.log("No URL");
-  }
   let queryKeys = Object.keys(req.query); // gets an array of all of the keys
   queryKeys.shift(); // removes url from that array
   let url = req.query.url;
-  console.log(url);
   if (queryKeys.length > 0) {
     queryKeys.forEach((key) => {
       url = url + `&${key}=${req.query[key]}`;
     });
   }
-  console.log(url);
-
-  if (cache[url] && Date.now() < cache[url].expires) {
-    res.json(cache[url].data);
+  if (isInCache(url)) {
+    console.log("Responding from Cache");
+    res.json(retriveFromCache(url));
   } else {
+    console.log("Getting new data");
     let response = await axios.get(url);
-    cache[url] = {
+
+    let data = {
       data: response.data,
       expires: Date.now() + TIME_TO_LIVE,
+      ip: req.ip,
     };
+    storeInCache(url, data);
+
     res.json(response.data);
   }
 });
@@ -43,3 +42,35 @@ const port = process.env.PORT || 6001;
 app.listen(port, () => {
   console.log("Back end is alive! + listening on " + port);
 });
+
+function isInCache(url) {
+  // Checks to see if the url is in the cache, and returns true or false
+  return cache[url] && Date.now() < cache[url].expires;
+}
+
+function retriveFromCache(url) {
+  // Using URL as key return data in the following format
+  // {
+  //   data: {data from request},
+  //   expires: times now + 5mins,
+  //   ip: users ip address
+  // }
+  return cache[url].data;
+}
+
+function storeInCache(url, data) {
+  // using the URL as the key, stores the data which is formatted as
+  // {
+  //   data: {data from request},
+  //   expires: times now + 5mins,
+  //   ip: users ip address
+  // }
+  // In future, will store our data in DB
+  cache[url] = data;
+}
+
+function getUsersCache(ip) {
+  let output = [];
+  // iterate over the cache and if IP matches, add that to output
+  return output;
+}
